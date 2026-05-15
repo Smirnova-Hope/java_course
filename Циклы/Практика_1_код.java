8d9cb4bc0ca944d18cd96ec8fa9646e3c73b92ce
Nadya <96446588+Smirnova-Hope@users.noreply.github.com>
2026-05-16 01:02:51 +0300

    Create Практика_1_код.java

---
+++ Циклы/Практика_1_код.java
@@ -0,0 +1,11 @@
+//  Бесконечный цикл while
+while (true) {
+    // тело цикла (может быть пустым или содержать логику)
+    System.out.println("Бесконечный цикл while");
+}
+
+// Бесконечный цикл do-while
+do {
+    // тело цикла
+    System.out.println("Бесконечный цикл do-while");
+} while (true);
